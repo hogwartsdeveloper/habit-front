feb999f42adf235906e23d094e6a3eaf540a0f48
zhannur <jannuraidynuly@gmail.com>
2023-03-25 05:57:26 +0600

    learn

--- src/app/pages/promo/learn/learn.component.ts
+++ src/app/pages/promo/learn/learn.component.ts
@@ -1,56 +1,39 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
 
 @Component({
   selector: 'app-learn',
   templateUrl: './learn.component.html',
-  styleUrls: ['./learn.component.scss']
+  styleUrls: ['./learn.component.scss'],
 })
 export class LearnComponent implements OnInit, AfterViewInit {
-  @ViewChild('main') canvas: ElementRef<HTMLCanvasElement>
   ngOnInit() {}
 
   ngAfterViewInit() {
-    const renderer = new THREE.WebGLRenderer({antialias: true, canvas: this.canvas.nativeElement});
-    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-    camera.position.z = 2;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      500
+    );
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
 
     const scene = new THREE.Scene();
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-    const cubes = [
-        this.makeInstance(geometry, 0x44aa88, 0, scene),
-        this.makeInstance(geometry, 0x8844aa, -2, scene),
-        this.makeInstance(geometry, 0xaa8844, 2, scene),
-    ];
+    const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const geometry = new THREE.BufferGeometry().setFromPoints([
+      new THREE.Vector3(-10, 0, 0),
+      new THREE.Vector3(0, 10, 0),
+      new THREE.Vector3(10, 0, 0),
+    ]);
 
-    const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-
-
-    function render(time: number) {
-      time *= 0.001;
-      cubes.forEach((cube, ndx) => {
-        const speed = 1 + ndx * .1;
-        const rot = time * speed;
-        cube.rotation.x = rot;
-        cube.rotation.y = rot;
-      });
-
-      renderer.render(scene, camera);
-      requestAnimationFrame(render);
-    }
-    requestAnimationFrame(render);
-  }
-
-  makeInstance(geometry: THREE.BoxGeometry, color: number, x: number, scene: THREE.Scene) {
-    const material = new THREE.MeshPhongMaterial({ color });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    cube.position.x = x;
-
-    return cube;
+    const line = new THREE.Line(geometry, material);
+    scene.add(line);
+    renderer.render(scene, camera);
   }
 }
