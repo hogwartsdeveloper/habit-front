c3d09c7ce0cab0441fa5e1ceb4aa348c8fab28b5
zhannur <jannuraidynuly@gmail.com>
2023-03-26 15:16:39 +0600

    add fantasticon

---
+++ src/assets/icons/custom-icons/icons.ts
@@ -0,0 +1,13 @@
+export type IconsId =
+  | 'instagram';
+
+export type IconsKey =
+  | 'Instagram';
+
+export enum Icons {
+  Instagram = 'instagram',
+}
+
+export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
+  [Icons.Instagram]: '61697',
+};
