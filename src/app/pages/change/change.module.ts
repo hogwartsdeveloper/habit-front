823c7c4a99db830f6a2b14f866212a4102e43aef
zhannur <jannuraidynuly@gmail.com>
2023-04-29 23:03:55 +0600

    change

--- src/app/pages/change/change.module.ts
+++ src/app/pages/change/change.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChangeComponent } from './change/change.component';
 import { ChangeRoutingModule } from './change-routing.module';
+import { ToolbarComponent } from '../../toolbar/toolbar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ChangeComponent],
-  imports: [CommonModule, ChangeRoutingModule],
+  imports: [
+    CommonModule,
+    ChangeRoutingModule,
+    ToolbarComponent,
+    MatButtonModule,
+  ],
 })
 export class ChangeModule {}
