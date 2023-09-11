c9a355ae117201eca6e92b260de7b78afd1312ed
zhannur <jannuraidynuly@gmail.com>
2023-09-11 18:00:00 +0600

    start activity chart

---
+++ src/app/charts/activity-chart/activity-chart.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-chart',
+  templateUrl: './activity-chart.component.html',
+  standalone: true,
+})
+export class ActivityChartComponent {}
