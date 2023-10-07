7318663ba58df73c67b614337433da18be04b9f6
zhannur <jannuraidynuly@gmail.com>
2023-10-07 21:32:46 +0600

    get habits

--- src/app/modules/habit/habit/habit.component.ts
+++ src/app/modules/habit/habit/habit.component.ts
@@ -1,36 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HabitService } from '../services/habit.service';
-import { Subject, takeUntil } from 'rxjs';
-import { IHabit } from '../models/habit.interface';
-import * as moment from 'moment/moment';
+import { IHabits } from '../models/habits.interface';
 
 @Component({
   selector: 'app-change',
   templateUrl: './habit.component.html',
   styleUrls: ['./habit.component.scss'],
 })
-export class HabitComponent implements OnInit, OnDestroy {
-  habits: IHabit[] = [];
-  destroy$ = new Subject();
+export class HabitComponent implements OnInit {
+  habits: IHabits;
   constructor(public habitService: HabitService) {}
 
   ngOnInit() {
-    this.habitService.habits$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((habits) => {
-        this.habits = habits.map((habit, id) => {
-          const lastActive = habit.lastActiveDate
-            ? moment(habit.lastActiveDate)
-            : moment();
-
-          habit.isOverdue = moment().diff(lastActive, 'days') > 1;
-          return habit;
-        });
-      });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(null);
-    this.destroy$.complete();
+    this.habitService
+      .get('651f80fc007ede299e36a86c')
+      .subscribe((habits) => (this.habits = habits));
   }
 }
