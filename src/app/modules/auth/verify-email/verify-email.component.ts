dd61d9696c943feaba37bff1f8d8b040cdcd0fa7
zhannur <jannuraidynuly@gmail.com>
2023-11-02 07:52:33 +0600

    verify email style component

--- src/app/modules/auth/verify-email/verify-email.component.ts
+++ src/app/modules/auth/verify-email/verify-email.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { IInput } from '../../../utils/ui/input/models/input.interface';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.scss']
+  styleUrls: ['./verify-email.component.scss'],
 })
 export class VerifyEmailComponent {
+  config = signal<IInput>({
+    title: 'Enter code',
+    placeholder: '',
+    required: true,
+    type: 'password',
+    fName: 'code',
+  });
 
+  form = signal<FormGroup>(
+    new FormGroup({ code: new FormControl(null, Validators.required) })
+  );
 }
