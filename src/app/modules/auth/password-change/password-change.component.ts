f4bf80e3a772bee920e6c1ed7a4a62cb13c6db15
zhannur <jannuraidynuly@gmail.com>
2023-11-10 10:44:37 +0600

    save password change

--- src/app/modules/auth/password-change/password-change.component.ts
+++ src/app/modules/auth/password-change/password-change.component.ts
@@ -1,14 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged, take } from 'rxjs';
 
 import { IInput } from '../../../utils/ui/input/models/input.interface';
+import { AuthApiService } from '../services/auth-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-password-change',
   templateUrl: './password-change.component.html',
   styleUrls: ['./password-change.component.scss'],
 })
-export class PasswordChangeComponent {
+export class PasswordChangeComponent implements OnInit {
   configs = signal<IInput[]>([
     {
       title: 'Password',
@@ -31,4 +35,33 @@ export class PasswordChangeComponent {
     })
   );
   email = signal<string>('jannuraidynuly@gmail.com');
+  isPasswordCoincide = signal<boolean>(false);
+  token: string;
+  destroyRef = inject(DestroyRef);
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly authApiService: AuthApiService
+  ) {}
+
+  ngOnInit() {
+    this.token = this.route.snapshot.paramMap.get('token')!;
+
+    this.form()
+      .valueChanges.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((values) => {
+        this.isPasswordCoincide.set(values.password === values.confirmPassword);
+      });
+  }
+
+  save() {
+    this.authApiService
+      .passwordChange(this.token, this.form().get('password')?.value)
+      .pipe(take(1))
+      .subscribe();
+  }
 }
