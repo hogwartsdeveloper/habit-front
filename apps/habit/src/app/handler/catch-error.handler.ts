1e39e1d3479ba0caed2303509d6f5bccf6950d5d
zhannur <jannuraidynuly@gmail.com>
2024-03-18 20:44:36 +0500

    HandleError

--- apps/habit/src/app/handler/catch-error.handler.ts
+++ apps/habit/src/app/handler/catch-error.handler.ts
@@ -1,12 +1,21 @@
-import { ErrorHandler, Injectable } from '@angular/core';
-import { MessageService } from 'ui';
+import {ErrorHandler, Injectable} from '@angular/core';
+import {MessageService} from 'ui';
+import {HttpErrorResponse} from "@angular/common/http";
+import {IError} from "../shared/models/api-result";
 
 @Injectable()
 export class CatchErrorHandler implements ErrorHandler {
   constructor(private readonly messageService: MessageService) {}
 
-  handleError(error: any) {
-    this.messageService.error(error?.error?.message);
-    throw new Error(error);
+  handleError(errorResponse: HttpErrorResponse) {
+    const errors: IError[] = errorResponse.error?.errors;
+    if (errors?.length) {
+      for (let item of errors) {
+        this.messageService.error(item.message)
+      }
+      return;
+    }
+
+    this.messageService.error(errorResponse.message);
   }
 }
